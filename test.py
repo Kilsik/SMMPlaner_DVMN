2d7673583a945bb321ca834dfa83c19b49ad0589
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-03-15 22:21:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -16,28 +16,29 @@ max_row = worksheet_smm.rows
 
 all_table_rows = worksheet_smm.range(f'{min_row}:{max_row}', returnas='cell')
 
+
 def get_parse_docx(path):
     doc = DocumentParser(path)
-    parse = []
+    text = []
     for _type, item in doc.parse():
         try:
-            parse.append(item['text'])
-        except:
+            text.append(item['text'])
+        except TypeError:
             img_data = item[1]['image']
             filename = item[1]['filename']
             text_img = img_data.split(',')[1]
             recovered = base64.b64decode(text_img)
             with open(filename, 'wb') as file:
                 file.write(recovered)
-    return parse
+    return ' '.join(text)
 
+print(get_parse_docx('post.docx'))
 def upload_docx(link):
     file_id = link.split('/')[-2]
     url = f"https://docs.google.com/document/d/{file_id}/export?format=docx&id={file_id}"
     response = requests.get(url)
     with open('post.docx', 'wb') as f:
         f.write(response.content)
-
 
 
 def get_rows_for_posts(all_table_rows):
@@ -52,7 +53,6 @@ def get_post_link(rows_for_post):
     for row in rows_for_post:
         post_link = row[SMM_GOOGLE_DOC].value
         return post_link
-
 
 
     # rows[SMM_TG].color = (1, 0, 0, 0)     # красим ячейки
