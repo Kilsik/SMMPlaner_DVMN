1db9a0af0d1b86e6c01804fcdb7c565d61711d7e
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-03-16 17:29:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -20,20 +20,20 @@ all_table_rows = worksheet_smm.range(f'{min_row}:{max_row}', returnas='cell')
 def get_parse_docx(path):
     doc = DocumentParser(path)
     text = []
+    filename = None
     for _type, item in doc.parse():
-        try:
+        if _type == 'paragraph':
             text.append(item['text'])
-        except TypeError:
-            img_data = item[1]['image']
+        elif _type == 'multipart':
+            img_data = item[1]['image'].split(',')[1]
             filename = item[1]['filename']
-            text_img = img_data.split(',')[1]
-            recovered = base64.b64decode(text_img)
+            recovered = base64.b64decode(img_data)
             with open(filename, 'wb') as file:
                 file.write(recovered)
     return ' '.join(text), filename
 
 
-def upload_docx(link):
+def download_docx(link):
     file_id = link.split('/')[-2]
     url = f"https://docs.google.com/document/d/{file_id}/export?format=docx&id={file_id}"
     response = requests.get(url)
@@ -43,9 +43,11 @@ def upload_docx(link):
 
 def get_rows_for_posts(all_table_rows):
     rows_for_post = []
-    for rows in all_table_rows:
-        if rows[SMM_GOOGLE_DOC].value:
-            rows_for_post.append(rows)
+    for row in all_table_rows:
+        if not row[SMM_GOOGLE_DOC].value and not row[SMM_GIF_LINK].value:
+            break
+        elif row[SMM_GOOGLE_DOC].value:
+            rows_for_post.append(row)
     return rows_for_post
 
 
@@ -55,7 +57,13 @@ def get_post_link(rows_for_post):
         return post_link
 
 
-    # rows[SMM_TG].color = (1, 0, 0, 0)     # красим ячейки
-    # rows[SMM_TG].value = False            # присваеваем значение
+def get_update_row(row, post_id, network='TG'):
+    row[globals()[f"SMM_{network}"]].color = (0, 1, 0, 0)
+    row[globals()[f"SMM_{network}"]].value = False
+    row[globals()[f"SMM_{network}_POST_ID"]].value = post_id
+
+
+
+
 
 # if __name__ == '__main__':
