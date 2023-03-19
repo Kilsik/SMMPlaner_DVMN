37044d28e6ba8cf60e0f75d9ecf239bacf565f6e
Igor <nekto.korobko@gmail.com>
2023-03-19 14:39:05 +0300

    move get_datetime to main refactoring pushing to social media

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from publish_on_ok import publish_to_ok, delete_ok_post
 from dotenv import load_dotenv
 
 
-from spreadsheets import (get_rows_for_posts, get_file, get_parsed_file, update_post_id, get_time_to_post, get_datetime,
+from spreadsheets import (get_rows_for_posts, get_file, get_parsed_file, update_post_id, get_time_to_post,
                           SMM_TG, SMM_OK, SMM_VK, SMM_DATE_POST, SMM_TIME_POST, SMM_DATE_ACTUAL_POST, SMM_GOOGLE_DOC,
                           SMM_IMAGE_LINK, SMM_TG_POST_ID, SMM_VK_POST_ID, SMM_OK_POST_ID, SMM_DELETE_POST)
 from publish_on_tg import send_post, send_animation_image
@@ -38,6 +38,11 @@ def format_text(text):
     formated_txt = formated_txt.replace('" ', '» ')
     formated_txt = formated_txt.replace(' - ', ' – ')
     return formated_txt
+
+
+def get_datetime(date, time='00:00:00'):
+    post_datetime = f"{date} {time}"
+    return datetime.datetime.strptime(post_datetime, '%d.%m.%Y %H:%M:%S')
 
 
 def main():
@@ -68,7 +73,7 @@ def main():
     rows_for_post, rows_for_delete = get_rows_for_posts(all_table_rows)
     date_now = datetime.datetime.now()
     today = date_now.date().strftime('%d.%m.%Y')
-    hour = date_now.strftime('%H:%M:00')
+    time_now = date_now.strftime('%H:%M:00')
 
     for row in rows_for_post:
         if not row[SMM_DATE_POST].value:
@@ -76,45 +81,42 @@ def main():
         else:
             date_post = row[SMM_DATE_POST].value
         if not row[SMM_TIME_POST].value:
-            time_post = hour
+            time_post = time_now
         else:
             time_post = row[SMM_TIME_POST].value
         datetime_post = get_datetime(date_post, time_post)
         if datetime_post > date_now:
             continue
-        file_link = ''
-        image_link = ''
+        text = ''
+        image = ''
+        gif_image = ''
         if row[SMM_GOOGLE_DOC].value:
             file_link = row[SMM_GOOGLE_DOC].value
-        else:
-            image_link = row[SMM_IMAGE_LINK].value
-        if file_link:
             downloaded_doc = get_file(file_link)
             text, image = get_parsed_file(downloaded_doc)
-            text = format_text(text)
-            if row[SMM_TG].value == 'TRUE' and not row[SMM_TG_POST_ID].value:
+        if row[SMM_GOOGLE_DOC].value and row[SMM_IMAGE_LINK].value:
+            image_link = row[SMM_IMAGE_LINK].value
+            gif_image = fetch_gif_image(image_link)
+        elif row[SMM_IMAGE_LINK].value:
+            image_link = row[SMM_IMAGE_LINK].value
+            gif_image = fetch_gif_image(image_link)
+        if row[SMM_TG].value != 'FALSE':
+            bot = telegram.Bot(token=telegram_token)
+            if (text and gif_image) or gif_image:
+                post_id = run(send_animation_image(telegram_chat_id, bot, gif_image, text))
+                update_post_id(row, post_id, network='TG')
+            else:
                 post_id = run(send_post(telegram_chat_id, bot, text, image))
                 update_post_id(row, post_id, network='TG')
-            if row[SMM_VK].value == 'TRUE' and not row[SMM_VK_POST_ID].value:
-                post_id = publish_to_vk(image, text, vk_token,
-                        vk_group_id, vk_ver)
-                update_post_id(row, post_id, network='VK')
-            if row[SMM_OK].value == 'TRUE' and not row[SMM_OK_POST_ID].value:
-                post_id = publish_to_ok(ok_app_key, ok_access_token, ok_sesion_key, ok_group_id, text, image)
-                update_post_id(row, post_id, network='OK')
-        elif image_link:
-            text = None
-            image = fetch_gif_image(image_link)
-            bot = telegram.Bot(token=telegram_token)
-            if row[SMM_TG].value == 'TRUE' and not row[SMM_TG_POST_ID].value:
-                post_id = run(send_animation_image(telegram_chat_id, bot, image))
-                update_post_id(row, post_id, network='TG')
-            if row[SMM_VK].value == 'TRUE' and not row[SMM_VK_POST_ID].value:
-                post_id = publish_to_vk(image, '', vk_token, vk_group_id, vk_ver)
-                update_post_id(row, post_id, network='VK')
-            if row[SMM_OK].value == 'TRUE' and not row[SMM_OK_POST_ID].value:
-                post_id = publish_to_ok(ok_app_key, ok_access_token, ok_sesion_key, ok_group_id, text, image)
-                update_post_id(row, post_id, network='OK')
+        if gif_image:
+            image = gif_image
+        if row[SMM_VK].value != 'FALSE':
+            post_id = publish_to_vk(image, text, vk_token,
+                                    vk_group_id, vk_ver)
+            update_post_id(row, post_id, network='VK')
+        if row[SMM_OK].value != 'FALSE':
+            post_id = publish_to_ok(ok_app_key, ok_access_token, ok_sesion_key, ok_group_id, text, image)
+            update_post_id(row, post_id, network='OK')
     for row in rows_for_delete:
         delete_date = row[SMM_DATE_ACTUAL_POST].value
         if delete_date > today:
